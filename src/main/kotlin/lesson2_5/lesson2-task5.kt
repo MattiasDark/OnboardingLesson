969b01dc9384dae920c3d629d74a4e16639b9237
MattiasDark <mattias00@yandex.ru>
2025-11-16 19:40:59 +0300

    KS-1-5 another change

--- src/main/kotlin/lesson2_5/lesson2-task5.kt
+++ src/main/kotlin/lesson2_5/lesson2-task5.kt
@@ -1,20 +1,15 @@
 package org.example.lesson2_5
-
+const val seconds_in_hour = 3600
+const val seconds_in_minute = 60
 fun main() {
     val secondsInSpace = 6480
     var hourInSpace = 0
     var minutesInSpace = 0
     var secondsInSpaceEnd = 0
 
-    hourInSpace = secondsInSpace / 3600
-    minutesInSpace = (secondsInSpace % 3600) / 60
-    secondsInSpaceEnd = secondsInSpace % 60
+    hourInSpace = secondsInSpace / seconds_in_hour
+    minutesInSpace = (secondsInSpace % seconds_in_hour) / seconds_in_minute
+    secondsInSpaceEnd = secondsInSpace % seconds_in_minute
 
-    println(hourInSpace)
-    println(minutesInSpace)
-    println(secondsInSpaceEnd)
-
-
-    println("Время в космосе: ${"%02d".format(hourInSpace)}:${"%02d".format(minutesInSpace)}:${"%02d".format(secondsInSpaceEnd)}")
-
+    println("Время в космосе: %02d:%02d:%02d".format(hourInSpace, minutesInSpace, secondsInSpaceEnd))
 }
