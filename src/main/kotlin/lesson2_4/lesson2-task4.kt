1e9013994f6703644955098ac828a3534d78ad77
MattiasDark <mattias00@yandex.ru>
2025-11-16 14:48:57 +0300

    OL-2-4 create test 4

---
+++ src/main/kotlin/lesson2_4/lesson2-task4.kt
@@ -0,0 +1,16 @@
+package org.example.lesson2_4
+
+fun main() {
+    val length: Long = 40868600000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Float =  2.0547945205479453E-4f
+    val apogee: Int = 327000
+    println("Рассотяние - $length")
+    println("Возраст - $age")
+    println("Часть дня - $partOfDay")
+    println("Секунды - $seconds")
+    println("Часть года - $partOfYear")
+    println("Апогей орбиты - $apogee")
+}
