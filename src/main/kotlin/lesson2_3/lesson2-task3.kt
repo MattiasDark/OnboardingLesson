496ed31896e1ec517100267224bfa4fb83f80b1f
MattiasDark <mattias00@yandex.ru>
2025-11-16 14:11:45 +0300

    OL-2-3 create test 3

---
+++ src/main/kotlin/lesson2_3/lesson2-task3.kt
@@ -0,0 +1,12 @@
+package org.example.lesson2_3
+
+fun main() {
+    val year = 1961
+    println("Год полета - $year")
+    var hour = 9
+    var minute = 7
+    println("Взлет - 0$hour:0$minute")
+    hour = 10
+    minute = 55
+    println("Посадка - $hour:$minute")
+}
