00d0ec0500eecb0ec2a40103775113dfeba2df7c
MattiasDark <mattias00@yandex.ru>
2025-11-16 13:20:18 +0300

    Initial Commit

--- src/main/kotlin/lesson_2/lesson_2_task_1.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -3,7 +3,7 @@ package org.example.lesson_2
 fun main() {
     val quantityOfOrders: Int = 75
     val thankYouMassage: String = "Спасибо за Ваш заказ. Ждем Вас ещё!"
-
+    //test
     println(quantityOfOrders)
     println(thankYouMassage)
 
