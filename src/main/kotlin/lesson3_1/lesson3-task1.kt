b4c63dc75ac8a056116b0121a575839005a1a21b
MattiasDark <mattias00@yandex.ru>
2025-11-18 22:06:24 +0300

    KS-3-1 initial commit

---
+++ src/main/kotlin/lesson3_1/lesson3-task1.kt
@@ -0,0 +1,12 @@
+package org.example.lesson3_1
+
+fun main() {
+    var welcomeMessage = ""
+    val name = "Roland of the Elda clan"
+
+    welcomeMessage = "Good day"
+    println(welcomeMessage + ", " + name)
+
+    welcomeMessage = "Good evening"
+    println(welcomeMessage + ", " + name)
+}
