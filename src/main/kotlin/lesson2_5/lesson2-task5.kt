09fc9f1998ea708b00db18581dff0358220e73f1
MattiasDark <mattias00@yandex.ru>
2025-11-19 20:54:57 +0300

    KS-2-5 some change

--- src/main/kotlin/lesson2_5/lesson2-task5.kt
+++ src/main/kotlin/lesson2_5/lesson2-task5.kt
@@ -4,7 +4,8 @@ import kotlin.math.pow
 
 fun main() {
     val firstMoney = 70000
-    val percent = 0.167
+    val truePercent = 16.7
+    val percent = truePercent/100
     val years = 20
 
     val compoundInterest: Double = firstMoney * ((1 + percent).pow(years))
